ccf13d4366fab7b750def690959a880b7f0a8013
abdulfastcode <abdul@fastcode.ai>
2024-01-07 14:55:52 +0530

    creating server

---
+++ index.js
@@ -0,0 +1,38 @@
+const http = require('http');
+
+let prodData = [{ id: 1, name: "iphone" }, { id: 2, name: "samsung" }, { id: 3, name: "google" }]
+
+function handler(req, res) {
+    // if(req.url == '/home'){
+    //     res.write("Hello World")
+    //     res.end()
+    // }
+    // if(req.url == '/products'){
+    //     res.write("products")
+    //     res.end()
+    // }
+    switch (req.url) {
+        case '/':
+            res.write("Hello World")
+            res.end()
+            break;
+        case '/products':
+            res.write(JSON.stringify(prodData))
+            res.end()
+            break;
+
+        default:
+            res.write("no route defined")
+            res.end()
+
+            break;
+    }
+
+}
+
+const server = http.createServer(handler)
+
+const port = 2000
+
+server.listen(port, () => { console.log('server running on port', port) })
+
